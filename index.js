6cfe863e9593f6decdf4e6a023059a8c123f3683
kenta <89286431+Kenta-Nakazaki-dev@users.noreply.github.com>
2024-03-12 10:16:24 +1030

    resizeable clocks

--- index.js
+++ index.js
@@ -5,49 +5,89 @@ const handleTimes = [
     id: "seconds-handle",
     toTimeValue: (time) => time.getSeconds(),
     angleDivide: 60,
+    handleLength: 0.8,
+    color: "black",
   },
   {
     id: "minutes-handle",
     toTimeValue: (time) => time.getMinutes(),
     angleDivide: 60,
+    handleLength: 0.7,
+    color: "blue",
   },
   {
     id: "hours-handle",
     toTimeValue: (time) => time.getHours(),
     angleDivide: 12,
+    handleLength: 0.5,
+    color: "red",
   },
 ];
-
-//
-//
-//
 
 const timeValueToAngle = (angleDivide, timeValue) => {
   const angle = (360 / angleDivide) * timeValue;
   return angle + clockAngleOffset;
 };
 
-const handleModifyAngle = (handleId) => (angle) => {
-  const handleElement = document.getElementById(handleId);
-  handleElement.style.transform = `rotate(${angle}deg)`;
+const drawClock = ({ size, id, time }) => {
+  const ctx = document.getElementById(id).getContext("2d");
+
+  const centerPoint = size / 2;
+
+  const handles = handleTimes.map((item) => ({
+    ...item,
+    angle: timeValueToAngle(item.angleDivide, item.toTimeValue(time)),
+  }));
+
+  ctx.clearRect(0, 0, size, size);
+  ctx.lineWidth = 5;
+
+  handles?.map((item) => {
+    const handLength = item.handleLength * (size / 2);
+
+    const angleInRadians = item.angle * (Math.PI / 180);
+
+    const xPos = handLength * Math.cos(angleInRadians) + centerPoint;
+    const yPos = handLength * Math.sin(angleInRadians) + centerPoint;
+
+    ctx.strokeStyle = item.color;
+
+    ctx.beginPath();
+    ctx.moveTo(centerPoint, centerPoint);
+    ctx.lineTo(xPos, yPos);
+
+    ctx.stroke();
+    ctx.closePath();
+  });
+
+  ctx.beginPath();
+  ctx.strokeStyle = "black";
+  ctx.arc(centerPoint, centerPoint, (size / 2) * 0.9, 0, 2 * Math.PI);
+  ctx.stroke();
 };
 
-const modifyTime = (time) =>
-  handleTimes.map((item) =>
-    handleModifyAngle(item.id)(
-      timeValueToAngle(item.angleDivide, item.toTimeValue(time))
-    )
-  );
+const createClock = (size) => (id) => {
+  const canvas = document.createElement("canvas");
+  canvas.id = id;
+  canvas.width = size;
+  canvas.height = size;
 
-//
-//initialize clock
-//
+  document.body.appendChild(canvas);
 
-let time = new Date();
-modifyTime(time);
+  drawClock({
+    size,
+    id,
+    time: new Date(),
+  });
+  setInterval(() => {
+    drawClock({
+      size,
+      id,
+      time: new Date(),
+    });
+  }, 1000);
+};
 
-setInterval(() => {
-  time = new Date();
+createClock(400)("xyzCanvas");
 
-  modifyTime(time);
-}, 1000);
+createClock(200)("canvas");
